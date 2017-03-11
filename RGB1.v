f887df581049f4df620b094981f7e50d9b39a514
dzv961 <dzv961@gmail.com>
2017-03-11 10:44:33 -0600

    Update RGB1.v

--- RGB1.v
+++ RGB1.v
@@ -1,24 +1,24 @@
 `timescale 1ns / 1ps
 
 
-module RGB1(
+module RGB1(  //inicia el modulo RGB, define sus entradas y salidas respectivas
     output wire [3:0] R,
     output wire [3:0] G,
     output wire [3:0] B,
-    input BotonR,
+    input BotonR,//BotonR, BotonG y BotonB son los switch donde el usuario puede escojer el color que desea
     input BotonG,
     input BotonB,
-    input wire BIT_FUENTE,
-    input wire H_ON,
+    input wire BIT_FUENTE,//es la salida del circuito de texto
+    input wire H_ON,//H_ON y V_ON nos indican si el pixel esta en la zona de display
     input wire V_ON,
     output  R2,
     output  G2,
     output  B2
     );
     
-    wire Tierra = 1'b0;
+    wire Tierra = 1'b0; 
     wire SalidaMUXR,SalidaMUXG,SalidaMUXB;
-    wire Seleccion =  BIT_FUENTE &  H_ON &  V_ON;
+    wire Seleccion =  BIT_FUENTE &  H_ON &  V_ON;//compuerta AND de BIT_FUENTE, H_ON y V_ON
     
    
     
